6d24433eb7fcbe51cbccac852715b7f7cf84ee4d
shanjay100 <shanjay@qq.com>
2020-04-02 18:03:16 +0800

    v20200402

--- studygo/day03/09bibao/main.go
+++ studygo/day03/09bibao/main.go
@@ -26,15 +26,17 @@ func f2(x, y int) {
 
 //要求：f1(f2)
 
-func f3(x, y int) func()  {
+func f3(f func(int, int), x, y int) func()  {
 	tmp := func() {
-		fmt.Println(x + y)
+		//fmt.Println(x + y)
+		////f2()
+		f(x, y)
 	}
 	return tmp
 }
 
 func main()  {
-	ret := f3(100, 200)
+	ret := f3(f2, 100, 200) //把原来需要传递两个int类型的参数包装成一个不需要传参的函数
 	f1(ret)
 }
 
